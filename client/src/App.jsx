fafb6edaa2d6ce6cb64175b4771b7bb699ccd36e
Phil <phillipmilton2023@gmail.com>
2024-04-11 14:46:32 -0700

    Landing page and learner homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,51 @@
-// App.js
+// import React from 'react';
+// import Registration from './Pages/Registration';
+// import Login from './Pages/Login';
+// import Home from './Pages/Home';
+// import {BrowserRouter, Route, Routes} from 'react-router-dom'
+
+
+// const App = () => {
+//   return (
+//    <BrowserRouter>
+//    <Routes>
+//     <Route path="/">
+//         <Route index element={<Registration />}/>
+//         <Route path="/login" element={<Login />}/>
+//         <Route path='/home' element={<Home />}/>
+//     </Route>
+//    </Routes>
+//    </BrowserRouter>
+//   );
+// };
+
+// export default App;
+
+
+
+
+
+
+
 import React from 'react';
 import Registration from './Pages/Registration';
 import Login from './Pages/Login';
 import Home from './Pages/Home';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-
+import Landing from './Pages/Landing'; 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-   <BrowserRouter>
-   <Routes>
-    <Route path="/">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
         <Route index element={<Registration />}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path='/home' element={<Home />}/>
-    </Route>
-   </Routes>
-   </BrowserRouter>
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function TopNavBar() {
+  return (
+    <div className="top-navbar">
+      <div className="logo">Student Name?</div>
+      <div className="spacer"></div>
+      <div className="settings">Settings</div>
+    </div>
+  );
+}
+
+
+function SideNavBar() {
+  const navigate = useNavigate(); // Hook for navigation
+
+  const handleLogout = () => {
+    // Navigate to the login page when logout button is clicked
+    navigate('/login');
+  };
+
+  return (
+    <div className="side-navbar">
+      <div className="course-title">Course 5</div>
+      <ul>
+        <li>Cards</li>
+        <li>Vocab</li>
+        <li>Videos</li>
+      </ul>
+      <button className="logout" onClick={handleLogout}>Logout</button>
+    </div>
+  );
+}
 
 function Home() {
   return (
-    <h1>
-        This will be the home page
-    </h1>
-  )
+    <div>
+      <TopNavBar />
+      <SideNavBar />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
