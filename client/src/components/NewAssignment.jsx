ecd84245751d296ab769f757d4415ae589b72282
Chance <chancemyoung@gmail.com>
2024-05-16 12:21:49 -0700

    Slightly updated, still experiencing problems

--- client/src/components/NewAssignment.jsx
+++ client/src/components/NewAssignment.jsx
@@ -3,200 +3,185 @@ import axios from 'axios';
 import { CohortContext } from '../context/cohortContext.jsx';
 
 const NewAssignment = () => {
-    const [questions, setQuestions] = useState([{ type: 'multiple-choice', questionText: '', answers: [{ text: '', correct: false }] }]);
-    const [assignmentName, setAssignmentName] = useState('');
-    const [assignmentType, setAssignmentType] = useState('');
-    const { cohort } = useContext(CohortContext);
-    const cohortID = cohort._id;
+  const [questions, setQuestions] = useState([{ type: 'multiple-choice', questionText: '', answers: [{ text: '', correct: false }] }]);
+  const [assignmentName, setAssignmentName] = useState('');
+  const [assignmentType, setAssignmentType] = useState('');
+  const { cohort } = useContext(CohortContext);
+  const cohortID = cohort._id;
 
-    const addQuestion = () => {
-        setQuestions([...questions, { type: 'multiple-choice', questionText: '', answers: [{ text: '', correct: false }] }]);
-    };
+  const addQuestion = () => {
+      setQuestions([...questions, { type: 'multiple-choice', questionText: '', answers: [{ text: '', correct: false }] }]);
+  };
 
-    const removeQuestion = (questionIndex) => {
-        setQuestions(questions.filter((_, index) => index !== questionIndex));
-    };
+  const removeQuestion = (questionIndex) => {
+      setQuestions(questions.filter((_, index) => index !== questionIndex));
+  };
 
-    const addAnswer = (questionIndex) => {
-        const newQuestions = [...questions];
-        newQuestions[questionIndex].answers.push({ text: '', correct: false });
-        setQuestions(newQuestions);
-    };
+  const addAnswer = (questionIndex) => {
+      const newQuestions = [...questions];
+      newQuestions[questionIndex].answers.push({ text: '', correct: false });
+      setQuestions(newQuestions);
+  };
 
-    const removeAnswer = (questionIndex, answerIndex) => {
-        const newQuestions = [...questions];
-        newQuestions[questionIndex].answers = newQuestions[questionIndex].answers.filter((_, index) => index !== answerIndex);
-        setQuestions(newQuestions);
-    };
+  const removeAnswer = (questionIndex, answerIndex) => {
+      const newQuestions = [...questions];
+      newQuestions[questionIndex].answers = newQuestions[questionIndex].answers.filter((_, index) => index !== answerIndex);
+      setQuestions(newQuestions);
+  };
 
-    const onChangeType = (e) => {
-        setAssignmentType(e.target.value);
-    };
+  const onChangeType = (e) => {
+      setAssignmentType(e.target.value);
+  };
 
-    const onChangeName = (e) => {
-        setAssignmentName(e.target.value);
-    };
+  const onChangeName = (e) => {
+      setAssignmentName(e.target.value);
+  };
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            console.log(cohortID);
-            const res = await axios.post('http://localhost:4000/newAssignment', {
-                cohortID: cohortID,
-                assignmentName: assignmentName,
-                questions: questions
-            });
-            console.log(res.data); // Handle successful registration
-            alert('Assignment was successfully created');
-        } catch (err) {
-            console.error('Assignment creation error:', err.response);
-        }
-    };
+  const onSubmit = async (e) => {
+      e.preventDefault();
+      try {
+          console.log(cohortID);
+          const res = await axios.post('http://localhost:4000/newAssignment', {
+              cohortID: cohortID,
+              assignmentName: assignmentName,
+              questions: questions
+          });
+          console.log(res.data); // Handle successful registration
+          alert('Assignment was successfully created');
+      } catch (err) {
+          console.error('Assignment creation error:', err.response);
+      }
+  };
 
-    const toggleCorrect = (questionIndex, answerIndex) => {
-        const newQuestions = [...questions];
-        newQuestions[questionIndex].answers[answerIndex].correct = !newQuestions[questionIndex].answers[answerIndex].correct;
-        setQuestions(newQuestions);
-    };
+  const toggleCorrect = (questionIndex, answerIndex) => {
+      const newQuestions = [...questions];
+      newQuestions[questionIndex].answers[answerIndex].correct = !newQuestions[questionIndex].answers[answerIndex].correct;
+      setQuestions(newQuestions);
+  };
 
-    const handleQuestionTypeChange = (index, type) => {
-        const newQuestions = [...questions];
-        newQuestions[index] = { type, questionText: '', answers: type === 'written-response' || type === 'essay' ? [] : [{ text: '', correct: false }] };
-        setQuestions(newQuestions);
-    };
+  const handleQuestionTypeChange = (index, type) => {
+      const newQuestions = [...questions];
+      newQuestions[index] = { type, questionText: '', answers: type === 'written-response' || type === 'essay' ? [] : [{ text: '', correct: false }] };
+      setQuestions(newQuestions);
+  };
 
-    return (
-        <div className='new-assignment-container'>
-            <div className='modal-content'>
-                <div className='formWrapper'>
-                    <span>New Assignment</span>
-                    <form onSubmit={onSubmit}>
-                        <div className="assignment-info">
-                            <div className="inputWrapper">
-                                <label htmlFor="assignmentName">Assignment Title:</label>
-                                <input type="text" id="assignmentName" name="assignmentName" value={assignmentName} onChange={onChangeName} required />
-                                <label htmlFor="assignmentType">Assignment Type:</label>
-                                <select id="assignmentType" name="assignmentType" value={assignmentType} onChange={onChangeType} required>
-                                    <option value="multiple-choice">Multi Choice</option>
-                                    <option value="true-false">True/False</option>
-                                    <option value="written-response">Response</option>
-                                    <option value="essay">Essay</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        {questions.map((question, questionIndex) => (
-                            <div key={questionIndex}>
-                                <select
-                                    value={question.type}
-                                    onChange={(e) => handleQuestionTypeChange(questionIndex, e.target.value)}
-                                >
-                                    <option value="multiple-choice">Multiple Choice</option>
-                                    <option value="true-false">True/False</option>
-                                    <option value="written-response">Written Response</option>
-                                    <option value="essay">Essay</option>
-                                </select>
-                                <input
-                                    type="text"
-                                    value={question.questionText}
-                                    onChange={(e) => {
-                                        const newQuestions = [...questions];
-                                        newQuestions[questionIndex].questionText = e.target.value;
-                                        setQuestions(newQuestions);
-                                    }}
-                                    placeholder={`Question #${questionIndex + 1}`}
-                                />
-                                <button type="button" onClick={() => removeQuestion(questionIndex)}>Remove Question</button>
-
-                                {question.type === 'multiple-choice' && question.answers.map((answer, answerIndex) => (
-                                    <div key={answerIndex}>
-                                        <input
-                                            type="text"
-                                            value={answer.text}
-                                            onChange={(e) => {
-                                                const newQuestions = [...questions];
-                                                newQuestions[questionIndex].answers[answerIndex].text = e.target.value;
-                                                setQuestions(newQuestions);
-                                            }}
-                                            placeholder={`Answer #${answerIndex + 1}`}
-                                        />
-                                        <input
-                                            type="checkbox"
-                                            checked={answer.correct}
-                                            onChange={() => toggleCorrect(questionIndex, answerIndex)}
-                                        />
-                                        <button type="button" onClick={() => removeAnswer(questionIndex, answerIndex)}>Remove Answer</button>
-                                    </div>
-                                ))}
-                                {question.type === 'multiple-choice' && (
-                                    <button type="button" onClick={() => addAnswer(questionIndex)}>Add Answer</button>
-                                )}
-
-                                {question.type === 'true-false' && (
-                                    <div>
-                                        <label>
-                                            <input
-                                                type="radio"
-                                                name={`trueFalse-${questionIndex}`}
-                                                value="true"
-                                                checked={question.answers[0]?.correct === true}
-                                                onChange={() => {
-                                                    const newQuestions = [...questions];
-                                                    newQuestions[questionIndex].answers = [{ text: 'True', correct: true }, { text: 'False', correct: false }];
-                                                    setQuestions(newQuestions);
-                                                }}
-                                            />
-                                            True
-                                        </label>
-                                        <label>
-                                            <input
-                                                type="radio"
-                                                name={`trueFalse-${questionIndex}`}
-                                                value="false"
-                                                checked={question.answers[0]?.correct === false}
-                                                onChange={() => {
-                                                    const newQuestions = [...questions];
-                                                    newQuestions[questionIndex].answers = [{ text: 'True', correct: false }, { text: 'False', correct: true }];
-                                                    setQuestions(newQuestions);
-                                                }}
-                                            />
-                                            False
-                                        </label>
-                                    </div>
-                                )}
-
-                                {question.type === 'written-response' && (
-                                    <textarea
-                                        value={question.answers[0]?.text || ''}
-                                        onChange={(e) => {
-                                            const newQuestions = [...questions];
-                                            newQuestions[questionIndex].answers = [{ text: e.target.value }];
-                                            setQuestions(newQuestions);
-                                        }}
-                                        placeholder="Enter the written response here"
-                                    />
-                                )}
-
-                                {question.type === 'essay' && (
-                                    <textarea
-                                        value={question.answers[0]?.text || ''}
-                                        onChange={(e) => {
-                                            const newQuestions = [...questions];
-                                            newQuestions[questionIndex].answers = [{ text: e.target.value }];
-                                            setQuestions(newQuestions);
-                                        }}
-                                        placeholder="Enter the essay response here"
-                                    />
-                                )}
-                            </div>
-                        ))}
-                        <button type="button" onClick={addQuestion}>Add Question</button>
-                        <button type="submit">Create Assignment</button>
-                    </form>
-                </div>
+  return (
+    <div className='new-assignment-container'>
+      <div className='modal-content'>
+        <div className='formWrapper'>
+          <span>New Assignment</span>
+          <form onSubmit={onSubmit}>
+            <div className="assignment-info">
+              <div className="inputWrapper">
+                <label htmlFor="assignmentName">Assignment Title:</label>
+                <input type="text" id="assignmentName" name="assignmentName" value={assignmentName} onChange={onChangeName} required />
+                <label htmlFor="assignmentType">Assignment Type:</label>
+                <select id="assignmentType" name="assignmentType" value={assignmentType} onChange={onChangeType} required>
+                  <option value="multiple-choice">Questions</option>
+                  <option value="essay">Essay</option>
+                </select>
+              </div>
             </div>
+
+            {questions.map((question, questionIndex) => (
+              <div key={questionIndex}>
+                  <select
+                    value={question.type}
+                    onChange={(e) => handleQuestionTypeChange(questionIndex, e.target.value)}
+                  >
+                    <option value="multiple-choice">Multiple Choice</option>
+                    <option value="true-false">True/False</option>
+                    <option value="written-response">Written Response</option>
+                  </select>
+                  <input
+                    type="text"
+                    value={question.questionText}
+                    onChange={(e) => {
+                      const newQuestions = [...questions];
+                      newQuestions[questionIndex].questionText = e.target.value;
+                      setQuestions(newQuestions);
+                    }}
+                    placeholder={`Question #${questionIndex + 1}`}
+                  />
+                  <button type="button" onClick={() => removeQuestion(questionIndex)}>Remove Question</button>
+
+                  {question.type === 'multiple-choice' && question.answers.map((answer, answerIndex) => (
+                    <div key={answerIndex}>
+                      <input
+                        type="text"
+                        value={answer.text}
+                        onChange={(e) => {
+                          const newQuestions = [...questions];
+                          newQuestions[questionIndex].answers[answerIndex].text = e.target.value;
+                          setQuestions(newQuestions);
+                        }}
+                        placeholder={`Answer #${answerIndex + 1}`}
+                      />
+                      <input
+                        type="checkbox"
+                        checked={answer.correct}
+                        onChange={() => toggleCorrect(questionIndex, answerIndex)}
+                      />
+                      <button type="button" onClick={() => removeAnswer(questionIndex, answerIndex)}>Remove Answer</button>
+                    </div>
+                  ))}
+                  {question.type === 'multiple-choice' && (
+                    <button type="button" onClick={() => addAnswer(questionIndex)}>Add Answer</button>
+                  )}
+
+                  {question.type === 'true-false' && (
+                    <div>
+                      <label>
+                        <input
+                          type="radio"
+                          name={`trueFalse-${questionIndex}`}
+                          value="true"
+                          checked={question.answers[0]?.correct === true}
+                          onChange={() => {
+                            const newQuestions = [...questions];
+                            newQuestions[questionIndex].answers = [{ text: 'True', correct: true }, { text: 'False', correct: false }];
+                            setQuestions(newQuestions);
+                          }}
+                        />
+                        True
+                      </label>
+                      <label>
+                        <input
+                          type="radio"
+                          name={`trueFalse-${questionIndex}`}
+                          value="false"
+                          checked={question.answers[0]?.correct === false}
+                          onChange={() => {
+                            const newQuestions = [...questions];
+                            newQuestions[questionIndex].answers = [{ text: 'True', correct: false }, { text: 'False', correct: true }];
+                            setQuestions(newQuestions);
+                          }}
+                        />
+                        False
+                      </label>
+                    </div>
+                  )}
+
+                  {/* {question.type === 'written-response' && (
+                    <textarea
+                      value={question.answers[0]?.text || ''}
+                      onChange={(e) => {
+                        const newQuestions = [...questions];
+                        newQuestions[questionIndex].answers = [{ text: e.target.value }];
+                        setQuestions(newQuestions);
+                      }}
+                      placeholder="Enter the written response here"
+                    />
+                  )} */}
+              </div>
+            ))}
+            <button type="button" onClick={addQuestion}>Add Question</button>
+            <button type="submit">Create Assignment</button>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NewAssignment;
